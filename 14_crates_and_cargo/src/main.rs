2c2b7802ab92c1398fb8ba99df05319e21151ba8
Daniel Kaschel <dipique@gmail.com>
2021-09-01 07:57:09 -0500

    Finish 14.1

--- 14_crates_and_cargo/src/main.rs
+++ 14_crates_and_cargo/src/main.rs
@@ -9,4 +9,15 @@ fn main() {
     //      stored in toml as package->license
     //      options listed in Linux Foundation SPDX list, e.g. MIT
     //      multiple licenses may be separated by OR, e.g. MIT OR Apache-2.0
+    // - There needs to actually be at least one line of code :p
+
+    // To publish, run: cargo publish
+    // To publish a new version, incremement version and run: cargo publish
+
+    // Remote specific version:
+    //   cargo yank --vers 1.0.1
+    // To undo a yank
+    //   cargo yank --vers 1.0.1 --undo
+
+    println!("Some code just in case that makes things happy");
 }
